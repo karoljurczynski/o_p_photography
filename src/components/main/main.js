28d5e5a993038a9fc14010458f0eb0b68be701f4
Karol <karol_jurczynski@o2.pl>
2021-03-01 14:00:38 +0100

    Hamburger menu style and transition added

--- src/components/main/main.js
+++ src/components/main/main.js
@@ -4,10 +4,29 @@ import '../../styles/components/main/main/main.css';
 import Picture from './picture';
 import PhotoReview from './photo_review';
 
+const menuIconTransition = () => {
+  const menuIcon = document.querySelector('.main__menu-icon');
+  const menuIconBars = menuIcon.children;
+
+  menuIcon.addEventListener("mouseover", () => {
+    menuIconBars[0].style.width = "40%";
+    menuIconBars[1].style.width = "100%";
+    menuIconBars[2].style.width = "55%";
+  });
+
+  menuIcon.addEventListener("mouseout", () => {
+    menuIconBars[0].style.width = "100%";
+    menuIconBars[1].style.width = "60%";
+    menuIconBars[2].style.width = "25%";
+  });
+};
 class Main extends React.Component {
   constructor(props) {
     super(props);
     this.state = {};
+  }
+  componentDidMount() {
+    menuIconTransition();
   }
 
   render() {
